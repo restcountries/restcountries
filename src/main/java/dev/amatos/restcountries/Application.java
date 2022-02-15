04af529616300b8e9219282dbb625ef0d06da531
Patrick Riegler <patrick.riegler@kreuzwerker.ch>
2022-02-15 17:52:01 +0100

    add micronaut openapi spec generation

--- src/main/java/dev/amatos/restcountries/Application.java
+++ src/main/java/dev/amatos/restcountries/Application.java
@@ -2,6 +2,21 @@ package dev.amatos.restcountries;
 
 import io.micronaut.runtime.Micronaut;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "rest-countries",
+                version = "3.1",
+                description = "Get information about countries via a RESTful API",
+                license = @License(name = "Mozilla Public License MPL 2.0", url = "https://www.mozilla.org/en-US/MPL/2.0/"),
+                contact = @Contact(url = "https://restcountries.com", name = "Alejandro Matos")
+        )
+)
 public class Application {
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
