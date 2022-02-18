591996e4bc4942d5708c6a415efe202b1cd1ad15
Patrick Riegler <patrick.riegler@kreuzwerker.ch>
2022-02-18 14:25:29 +0100

    remove micronaut openapi spec generation for previous versions and add / in front of controller paths to make the controller definition openapi compatible

--- src/main/java/dev/amatos/restcountries/controller/CountryControllerV31.java
+++ src/main/java/dev/amatos/restcountries/controller/CountryControllerV31.java
@@ -22,7 +22,7 @@ import java.util.Set;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-@Controller("v3.1/")
+@Controller("/v3.1/")
 public class CountryControllerV31 extends ControllerHelper {
 
   @Get(uri = "all", produces = MediaType.APPLICATION_JSON)
